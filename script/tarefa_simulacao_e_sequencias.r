4b1fa60ba6dd4290d6d45a4474173a51d3d26376
Caio Malaquias <caio.malaquias@ufpe.br>
2021-04-19 19:40:35 -0300

    simulação e sequências

--- script/tarefa_simulacao_e_sequencias.r
+++ script/tarefa_simulacao_e_sequencias.r
@@ -1,2 +1,7 @@
-#~~cria data frame sem nada dentro
-dfVazio <- data.frame()
+semente <- addTaskCallback(function(...) {set.seed(99);TRUE}) #cria/roda semente
+
+varNormal <- rnorm(99) # variável normal
+varBinomial <- rbinom(99, 9, .9) # variável binomial
+varIndex <- seq(1, length(varBinomial))# variável indexadora
+
+removeTaskCallback(semente) # para semente

b5cc30b21dd975a8b4f46e3e07fa8d02ce3da884
Caio Malaquias <caio.malaquias@ufpe.br>
2021-04-19 19:39:30 -0300

    Data frame

--- script/tarefa_criar_data_frame.r
+++ script/tarefa_criar_data_frame.r
@@ -1,2 +1,6 @@
-#~~cria data frame sem nada dentro
-dfVazio <- data.frame()
+semente <- addTaskCallback(function(...) {set.seed(99);TRUE}) #cria/roda semente
+#~~cria data frame de cadastro
+Cadastro <- data.frame(ID = sample(c(000:999), 100, replace = TRUE),
+                       genero = sample(c("Masc", "Fem"), 100, replace = TRUE),
+                       idade = rnorm(n = 100, mean = 25, sd = 5))
+removeTaskCallback(semente) # para semente

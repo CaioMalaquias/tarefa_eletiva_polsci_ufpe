0917c86843ac8aa1e39b4046f220a2f7472a522c
Caio Malaquias <caio.malaquias@ufpe.br>
2021-05-16 21:57:31 -0300

    large_data

--- script/large_data.r
+++ script/large_data.r
@@ -1,28 +1,20 @@
-library(data.table)
+install.packages("ff")
+install.packages("ffbase")
 
-setwd("C:/Users/CaioM/github/etl_com_r")
+library(ff)
+library(ffbase)
 
-# cria dado com mais de 1 GB
-Cadastro <-
-  data.frame(
-    genero = sample(c("Masc", "Fem"), 40000000, replace = TRUE),
-    idade = rnorm(n = 40000000, mean = 25, sd = 5),
-    salario = rnorm(n = 40000000, mean = 1200, sd = 20),
-    filhosDummy = sample(c("Sim", "Não"), 40000000, replace = TRUE),
-    covidDummy = rbinom(40000000, 1, .2))
+enderecoBase <- 'bases_originais/largeData.csv'
 
-write.csv2(Cadastro, "bases_originais/CadastroCovid.csv")
+# criando o arquivo ff
+extracaoLD4 <- read.csv.ffdf(file=enderecoBase)
 
-# extração direta via read.csv2
-system.time(extracaoLD1 <- read.csv2("bases_originais/CadastroCovid.csv"))
-object.size(extracaoLD1)
+sum(extracaoLD4[,1])
+mean(extracaoLD4[,2])
+sqrt(extracaoLD4[,3])# aqui é matemática individual
 
-# extração via amostragem com read.csv
-amostraLD2 <- read.csv2("bases_originais/fobhav.csv", nrows = 20)
-amostraLD2Classes <- sapply(amostraLD1, class) # encontra a classe da amostra amostra
-system.time(extracaoLD2 <- data.frame(read.csv2("bases_originais/CadastroCovid.csv", colClasses=amostraLD1Classes)))
-object.size(extracaoLD2)
-
-# extração via função fread, que já faz amostragem automaticamente
-system.time(extracaoLD3 <- fread("bases_originais/CadastroCovid.csv"))
-object.size(extracaoLD3)
+# amostra com 180 casos. 30 para cada coeficiente da regressão futura
+# ia ser massa fazer um bootstrap disso. acho que vai ser a próxima aula
+LD4sample180 <- extracaoLD4[sample(nrow(extracaoLD4), 180) , ]
+#regressão
+lm(c ~ ., LD4sample180)
